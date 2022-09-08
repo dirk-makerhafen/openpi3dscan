0216f6ea421e474ab552d98b3e5374794f00ac74
Dirk Makerhafen <dirk@makerhafen.de>
2022-09-08 23:17:04 +0200

    updates

--- server/views/appView.py
+++ server/views/appView.py
@@ -10,14 +10,24 @@ from .devicesView import DevicesView
 from .liveView import LiveView
 from .sidebar.sidebarView import SidebarView
 
+class CurrentView(PyHtmlView):
+    TEMPLATE_STR = '''{{ pyview.current_view.render()}}'''
+    def __init__(self, subject, parent, current_view):
+        super().__init__(subject, parent)
+        self.current_view = current_view
+
+    def set_view(self, new_view):
+        if self.current_view != new_view:
+            self.current_view = new_view
+            self.update()
 
 class AppView(PyHtmlView):
     DOM_ELEMENT_CLASS = "AppView container"
     TEMPLATE_STR = '''
         {% if pyview.subject.status == "active"%}
             <div class="row" >
-                {{ pyview.sidebar.render() }}
-                {{ pyview.current_view.render()}}
+                {{ pyview.sidebarView.render() }}
+                {{ pyview.currentView.render()}}
             </div>
         {% else %}
             <div style="width:100%;text-align:center;font-size:3em;padding-top: 20%;color:#aaa">
@@ -40,30 +50,23 @@ class AppView(PyHtmlView):
 
     def __init__(self, subject: App, parent):
         super().__init__(subject, parent)
-        self.sidebar = SidebarView(subject=subject, parent=self)
-        self.devices = DevicesView(subject=subject.devices, parent=self)
+        self.sidebarView = SidebarView(subject=subject, parent=self)
+        self.devicesView = DevicesView(subject=subject.devices, parent=self)
         self.liveView = LiveView(subject=subject, parent=self)
         self.shotView = ShotView(subject=subject, parent=self)
-        self.settings = SettingsView(self.subject, self)
-        self.current_view = self.devices
+        self.settingsView = SettingsView(self.subject, self)
+        self.currentView = CurrentView(subject, self, self.devicesView)
 
     def show_devicesView(self):
-        if self.current_view != self.devices:
-            self.current_view = self.devices
-            self.update()
+        self.currentView.set_view(self.devicesView)
 
     def show_settingsView(self):
-        if self.current_view != self.settings:
-            self.current_view = self.settings
-            self.update()
+        self.currentView.set_view(self.settingsView)
 
     def show_liveView(self):
-        if self.current_view != self.liveView:
-            self.current_view = self.liveView
-            self.update()
+        self.currentView.set_view(self.liveView)
 
     def show_shotView(self, shot):
         self.shotView.show_shot(shot)
-        if self.current_view != self.shotView:
-            self.current_view = self.shotView
-            self.update()
+        self.currentView.set_view(self.shotView)
+

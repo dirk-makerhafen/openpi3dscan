187091e44e657b9515479a302fe0e37431d50dc5
Dirk Makerhafen <dirk@makerhafen.de>
2022-09-25 01:09:37 +0200

    updates

--- server/app/settings/settings.py
+++ server/app/settings/settings.py
@@ -9,7 +9,7 @@ from app.settings.settingsSequence import SettingsSequence
 from app.settings.settingsWireless import SettingsWireless
 from app.settings.settingsRealityCapture import SettingsRealityCapture
 
-VERSION = "2022.09.24-08.06"
+VERSION = "2022.09.24-08.07"
 
 
 class Settings(Observable):

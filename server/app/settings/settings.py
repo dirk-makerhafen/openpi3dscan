205c80d749d0c4f5faa501dc2e60a0c65423c3e4
Dirk Makerhafen <dirk@makerhafen.de>
2022-11-02 02:50:52 +0100

    updates

--- server/app/settings/settings.py
+++ server/app/settings/settings.py
@@ -9,7 +9,7 @@ from app.settings.settingsSequence import SettingsSequence
 from app.settings.settingsWireless import SettingsWireless
 from app.settings.settingsRealityCapture import SettingsRealityCapture
 
-VERSION = "2022.11.02-00.35"
+VERSION = "2022.11.02-00.36"
 
 
 class Settings(Observable):
--- server/app/settings/settingsCameras.py
+++ server/app/settings/settingsCameras.py
@@ -84,7 +84,7 @@ class SettingsCameras(Observable):
         if self.shutter_speed_adjustment < 0:
             maxadj = min(self._per_segment_shutter_speeds) - 5000
             if maxadj < 0: maxadj = 0
-            adj = -(maxadj / 100.0 * self.shutter_speed_adjustment)
+            adj = maxadj / 100.0 * self.shutter_speed_adjustment
         return [x + adj for x in self._per_segment_shutter_speeds]
 
     def _set_per_segment_shutter_speeds(self, values):

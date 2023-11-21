5586cfe408280691046aa8d3426c151abc6da017
Dirk Makerhafen <dirk@makerhafen.de>
2023-11-21 19:20:41 +0100

    updates

--- server/app/settings/settings.py
+++ server/app/settings/settings.py
@@ -10,7 +10,7 @@ from app.settings.settingsWireless import SettingsWireless
 from app.settings.settingsDropbox import SettingsDropbox
 from app.settings.settingsRealityCapture import SettingsRealityCapture
 
-VERSION = "2023.11.01-22:22"
+VERSION = "2023.11.21-18:35"
 
 class Settings(Observable):
     def __init__(self, devicesInstance):

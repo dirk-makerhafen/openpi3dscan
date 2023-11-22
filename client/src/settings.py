80fe0053e9e18c5cdbbd5d57362d5008c976645e
Dirk Makerhafen <dirk@makerhafen.de>
2023-11-23 00:27:22 +0100

    updates

--- client/src/settings.py
+++ client/src/settings.py
@@ -7,7 +7,7 @@ class Settings():
         self.ID = "%s" % random.randint(1000, 99999)
         self.TYPE = "camera"
         self.NAME = "noname"
-        self.VERSION = "2023.11.21-18:35"
+        self.VERSION = "2023.11.23-00:23"
         self.ROTATION = 0
         self.load()
 
